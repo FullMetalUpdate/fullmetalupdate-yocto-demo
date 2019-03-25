57d4f00e0f67c497b16dabc984135e8d3e603cbf
cvincent <cvincent@witekio.com>
2019-03-25 11:34:40 -0700

    config: Add a new script to automate the configuration of the build system

---
+++ ConfigureBuild.sh
@@ -0,0 +1,27 @@
+sed_escape() {
+  sed -e 's/[]\/$*.^[]/\\&/g'
+}
+
+cfg_write() { # path, key, value
+  cfg_delete "$1" "$2"
+  echo "$2 = $3" >> "$1"
+}
+
+cfg_delete() { # path, key
+  test -f "$1" && sed -i "/^$(echo $2 | sed_escape).*$/d" "$1"
+}
+
+if [ -e ./config.cfg ]; then
+	echo 'config.cfg already exits. Do you really want to overwrite it? Please type yes to confirm.'
+    read -n 3 -p "> " ans;
+
+    case $ans in
+        'yes')
+        ;;
+        *)
+            exit;;
+    esac
+fi
+
+cp ./config.cfg.sample ./config.cfg
+cfg_write config.cfg server_host_name $(hostname)
